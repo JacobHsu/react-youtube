12bd9747c57c260be513de9222beceb2e2b80bf7
jacobhsu <jacob.hsu.tw@gmail.com>
2020-07-14 10:14:34 +0800

    App.js class App extends React.Component

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
